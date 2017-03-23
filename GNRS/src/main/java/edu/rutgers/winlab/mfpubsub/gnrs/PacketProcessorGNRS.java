2de6be5059e82b23c3a5c64a663e850f940f39d4
zoe <zoe@ubuntu>
2017-03-23 18:45:59 -0400

    GNRS processor

--- GNRS/src/main/java/edu/rutgers/winlab/mfpubsub/gnrs/PacketProcessorGNRS.java
+++ GNRS/src/main/java/edu/rutgers/winlab/mfpubsub/gnrs/PacketProcessorGNRS.java
@@ -8,6 +8,9 @@ package edu.rutgers.winlab.mfpubsub.gnrs;
 import edu.rutgers.winlab.mfpubsub.common.elements.NetworkInterface;
 import edu.rutgers.winlab.mfpubsub.common.elements.PacketProcessor;
 import edu.rutgers.winlab.mfpubsub.common.packets.MFPacket;
+import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketGNRS;
+import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketGNRSPayloadAssoc;
+import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketGNRSPayloadQuery;
 import edu.rutgers.winlab.mfpubsub.common.structure.Address;
 import edu.rutgers.winlab.mfpubsub.common.structure.GUID;
 import edu.rutgers.winlab.mfpubsub.common.structure.NA;
@@ -19,7 +22,8 @@ import java.util.List;
  *
  * @author zoe
  */
-public class PacketProcessorGNRS extends PacketProcessor{
+public class PacketProcessorGNRS extends PacketProcessor {
+
     private HashMap<GUID, List<Address>> Table;
 
     public PacketProcessorGNRS(NA myNA, HashMap<NA, NetworkInterface> neighbors) {
@@ -28,7 +32,18 @@ public class PacketProcessorGNRS extends PacketProcessor{
 
     @Override
     protected void handlePacket(MFPacket packet) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (packet.getType() == MFPacketGNRS.MF_PACKET_TYPE_GNRS) {
+            MFPacketGNRS pkt = (MFPacketGNRS) packet;
+            if(pkt.getPayload().getType() == MFPacketGNRSPayloadQuery.MF_GNRS_PACKET_PAYLOAD_TYPE_QUERY){
+                //TODO: look up the GNRS table and send response
+            }
+            else if(pkt.getPayload().getType() == MFPacketGNRSPayloadAssoc.MF_GNRS_PACKET_PAYLOAD_TYPE_ASSOC){
+                //TODO: add the subscriber GUID to the mapping of topic GUID if existed
+                //TODO: renew the multicast tree and multicast GNRS sync
+            }
+        } else {
+            System.out.println("This is not the correct packet type that GNRS should receive.");
+        }
     }
-    
+
 }
