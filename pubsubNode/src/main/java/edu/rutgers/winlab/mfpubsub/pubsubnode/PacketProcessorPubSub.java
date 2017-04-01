c3499b9d5fe914ef4c3118fb88871d7ef9addfae
zoe <zoe@ubuntu>
2017-03-31 22:30:27 -0400

    pubsub node process renew

--- pubsubNode/src/main/java/edu/rutgers/winlab/mfpubsub/pubsubnode/PacketProcessorPubSub.java
+++ pubsubNode/src/main/java/edu/rutgers/winlab/mfpubsub/pubsubnode/PacketProcessorPubSub.java
@@ -8,6 +8,9 @@ package edu.rutgers.winlab.mfpubsub.pubsubnode;
 import edu.rutgers.winlab.mfpubsub.common.elements.NetworkInterface;
 import edu.rutgers.winlab.mfpubsub.common.elements.PacketProcessor;
 import edu.rutgers.winlab.mfpubsub.common.packets.MFPacket;
+import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketData;
+import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketGNRS;
+import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketGNRSPayload;
 import edu.rutgers.winlab.mfpubsub.common.structure.NA;
 import java.io.IOException;
 import java.util.HashMap;
@@ -16,14 +19,27 @@ import java.util.HashMap;
  *
  * @author zoe
  */
-public class PacketProcessorPubSub extends PacketProcessor{
+public class PacketProcessorPubSub extends PacketProcessor {
 
-    public PacketProcessorPubSub(NA myNA, HashMap<NA, NetworkInterface> neighbors) {
+    private final HashMap<NA, HashMap<NA, Integer>> weightGraph;
+
+    private final DijkstraTree dijkstraGraph;
+    //guid - na table/process
+    public PacketProcessorPubSub(HashMap<NA, HashMap<NA, Integer>> weightGraph, NA myNA, HashMap<NA, NetworkInterface> neighbors) {
         super(myNA, neighbors);
+        this.weightGraph = weightGraph;
+        this.dijkstraGraph = new DijkstraTree(this.weightGraph);
     }
 
     @Override
     protected void handlePacket(MFPacket packet) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        switch (packet.getType()) {
+            case MFPacketGNRS.MF_PACKET_TYPE_GNRS:
+                break;
+            case MFPacketData.MF_PACKET_TYPE_DATA:
+                break;
+            default:
+                System.err.println("receive a wrong packet type which shouldn't receive actually.");
+        }
     }
 }
