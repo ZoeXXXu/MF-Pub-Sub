d6a87c2aeeeed208702840d46fd2bd08100f49d5
zoe <zoe@zoe-VirtualBox>
2017-04-02 17:25:18 -0400

    renew: sub add branch process

--- pubsubNode/src/main/java/edu/rutgers/winlab/mfpubsub/pubsubnode/PacketProcessorPubSub.java
+++ pubsubNode/src/main/java/edu/rutgers/winlab/mfpubsub/pubsubnode/PacketProcessorPubSub.java
@@ -9,6 +9,7 @@ import edu.rutgers.winlab.mfpubsub.common.elements.NetworkInterface;
 import edu.rutgers.winlab.mfpubsub.common.elements.PacketProcessor;
 import edu.rutgers.winlab.mfpubsub.common.packets.MFPacket;
 import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketData;
+import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketDataPayloadSub;
 import edu.rutgers.winlab.mfpubsub.common.packets.MFPacketGNRS;
 import edu.rutgers.winlab.mfpubsub.common.structure.Address;
 import edu.rutgers.winlab.mfpubsub.common.structure.GUID;
@@ -23,31 +24,83 @@ import java.util.HashMap;
  */
 public class PacketProcessorPubSub extends PacketProcessor {
 
-    private final HashMap<NA, HashMap<NA, Integer>> weightGraph;
+    private final GUID PubSubGUID;
+
+    private final HashMap<NA, HashMap<NA, Integer>> coreWeightGraph;
 
     private final DijkstraTree dijkstraGraph;
 
     //this two should be updated synchronization
     private final HashMap<GUID, HashMap<NA, ArrayList<Address>>> multiTree = new HashMap<>();
 
+    //this NA is the NA of connected router, not hosts' NA
+    private final HashMap<GUID, NA> RoutingTable;
+
     private final HashMap<GUID, NA> RPs = new HashMap<>();
 
+    private final HashMap<GUID, ArrayList<GUID>> GraphTable;
+
     //guid - na table/process
-    public PacketProcessorPubSub(HashMap<NA, HashMap<NA, Integer>> weightGraph, NA myNA, HashMap<NA, NetworkInterface> neighbors) {
+    public PacketProcessorPubSub(GUID PubSub, HashMap<NA, HashMap<NA, Integer>> weightGraph, HashMap<GUID, NA> RoutingTable, HashMap<GUID, ArrayList<GUID>> GraphTable, NA myNA, HashMap<NA, NetworkInterface> neighbors) {
         super(myNA, neighbors);
-        this.weightGraph = weightGraph;
-        this.dijkstraGraph = new DijkstraTree(this.weightGraph);
+        this.PubSubGUID = PubSub;
+        this.coreWeightGraph = weightGraph;
+        this.RoutingTable = RoutingTable;
+        this.GraphTable = GraphTable;
+        this.dijkstraGraph = new DijkstraTree(this.coreWeightGraph);
     }
 
     @Override
     protected void handlePacket(MFPacket packet) throws IOException {
-        switch (packet.getType()) {
-            case MFPacketGNRS.MF_PACKET_TYPE_GNRS:
-                break;
-            case MFPacketData.MF_PACKET_TYPE_DATA:
-                break;
-            default:
+        if (packet.getType() == MFPacketData.MF_PACKET_TYPE_DATA) {
+            MFPacketData pkt = (MFPacketData) packet;
+            if (pkt.getSID() == MFPacketDataPayloadSub.MF_PACKET_DATA_SID_SUBSCRIPTION) {
+                //update the related tree
+                AddBranch(pkt);
+            } else {
                 System.err.println("receive a wrong packet type which shouldn't receive actually.");
+            }
+        } else {
+            System.err.println("receive a wrong packet type which shouldn't receive actually.");
         }
+    }
+
+    private void AddBranch(MFPacketData pkt) {
+        MFPacketDataPayloadSub sub = (MFPacketDataPayloadSub) pkt.getPayload();
+        NA rp = getRP(sub);
+        GraphAdd(sub.getTopicGUID(), pkt.getsrcGuid());
+        NA router = RoutingTable.get(pkt.getsrcGuid());
+    }
+
+    private void RenewTree(GUID topicGUID) {
+
+    }
+
+    private void NotifyGNRS(MFPacketData pkt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void TreeBuild(GUID topicGUID) {
+
+    }
+    
+    private ArrayList<NA> GUIDtoNA(GUID topic){
+        return null;
+    }
+    
+    private NA getRP(MFPacketDataPayloadSub sub) {
+        NA rp = RPs.get(sub.getTopicGUID());
+        if (rp != null) {
+            return rp;
+        }
+        throw new IllegalStateException(String.format("PubSub Node cannot find RP of topic %s", sub.getTopicGUID().print(System.out)));
+    }
+    
+    private void GraphAdd(GUID key, GUID value){
+        ArrayList<GUID> tmp = GraphTable.get(key);
+        if(tmp == null){
+            GraphTable.put(key, tmp = new ArrayList<>());
+        }
+        tmp.add(value);
     }
 }
