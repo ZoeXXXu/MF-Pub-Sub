5aca36d8385538ca9a8ddb441895d59ff80d98b5
zoe <zoe@zoe-VirtualBox>
2017-04-06 17:24:29 -0400

    topoManager

--- topoManager/src/main/java/edu/rutgers/winlab/mfpubsub/topomanager/TopologyManager.java
+++ topoManager/src/main/java/edu/rutgers/winlab/mfpubsub/topomanager/TopologyManager.java
@@ -5,10 +5,28 @@
  */
 package edu.rutgers.winlab.mfpubsub.topomanager;
 
+import edu.rutgers.winlab.mfpubsub.common.structure.GUID;
+import edu.rutgers.winlab.mfpubsub.common.structure.NA;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author zoe
  */
 public class TopologyManager {
     
+    private static final HashMap<NA, ArrayList<NA>> topology = new HashMap<>();
+
+    public TopologyManager() {
+        ArrayList<NA> neighbor;
+//        topology.put(, value)
+
+    }
+    
+    private GUID createGUID(byte num){
+        byte[] buf = new byte[GUID.GUID_LENGTH];
+        buf[GUID.GUID_LENGTH - 1] = num;
+        return new GUID(buf);
+    }
 }
