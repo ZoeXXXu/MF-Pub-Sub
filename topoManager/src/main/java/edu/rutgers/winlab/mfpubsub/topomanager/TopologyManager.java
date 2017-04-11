474ffdbcc3ed3028872db41332056f7b87724ace
zoe <zoe@zoe-VirtualBox>
2017-04-11 15:33:31 -0400

    topoManager outline

--- topoManager/src/main/java/edu/rutgers/winlab/mfpubsub/topomanager/TopologyManager.java
+++ topoManager/src/main/java/edu/rutgers/winlab/mfpubsub/topomanager/TopologyManager.java
@@ -6,6 +6,7 @@
 package edu.rutgers.winlab.mfpubsub.topomanager;
 
 import edu.rutgers.winlab.mfpubsub.common.elements.NetworkInterface;
+import edu.rutgers.winlab.mfpubsub.common.elements.NetworkInterfaceUDP;
 import edu.rutgers.winlab.mfpubsub.common.elements.PacketProcessor;
 import edu.rutgers.winlab.mfpubsub.common.packets.MFPacket;
 import edu.rutgers.winlab.mfpubsub.common.structure.GUID;
@@ -15,7 +16,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -24,75 +28,147 @@ import java.util.HashMap;
 public class TopologyManager {
 
 //    private final PacketProcessor processor;
-private final PacketProcessorTopologyManager manager;
-    public TopologyManager(NA self, String filename, String tracefile, String ID) throws IOException {
-        manager = new PacketProcessorTopologyManager(readTopo(self, filename, ID), tracefile, self, neighbors);
+    private final PacketProcessorTopologyManager manager;
+    private final NA self;
+    private GUID GNRSguid;
+    private NA GNRSna;
+    private GUID PubSubguid;
+    private NA PubSubna;
+    private HashMap<NA, ArrayList<NA>> neighbors = new HashMap<>();
+
+    public TopologyManager(NA self, String filename, String tracefile, String ID, int port, String selfIP, String ctrlIP) throws IOException {
+//        PacketProcessor pro = readTopo(self, filename, ID);
+        this.self = self;
+        HashMap<NA, NetworkInterface> neighbor = new HashMap<>();
+        neighbor.put(new NA(999999), new NetworkInterfaceUDP(new InetSocketAddress(selfIP, port), new InetSocketAddress(ctrlIP, port)));
+        manager = new PacketProcessorTopologyManager(neighbor, readTopo(filename, ID), tracefile);
     }
 
     private GUID createGUID(byte[] guid) throws IOException {
-        if (guid.length < GUID.GUID_LENGTH) {
+        if (guid.length == GUID.GUID_LENGTH) {
             return new GUID(guid);
         } else {
-            throw new IOException("GUID length is too long");
+            throw new IOException("GUID length is not fittable");
         }
     }
 
-    private PacketProcessor readTopo(NA self, String filename, String ID) throws FileNotFoundException, IOException {
-//        BufferedReader buf = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
-//        int port_ini_num = 10000;
+    private PacketProcessor readTopo(String filename, String ID) throws FileNotFoundException, IOException {
         BufferedReader buf = new BufferedReader(new FileReader(filename));
-        switch (ID) {
-            case "router":
-                return doRouter(self, buf);
-            case "endhost":
-                return doEndHost(self, buf);
-            case "GNRS":
-                return doGNRS(self, buf);
-            case "PubSub Node":
-                return doPubSubNode(self, buf);
-            default:
-                throw new IOException("don't  understand which element you want to create");
+        String[] address;
+        HashMap<String, String> NAIP = new HashMap<>(); // NA - IP
+        while ((address = buf.readLine().split(" ")) != null) {
+            initial(address, buf);
+            switch (ID) {
+                case "router":
+                    return doRouter(NAIP, buf);
+                case "endhost":
+                    return doEndHost(NAIP, self, buf);
+                case "GNRS":
+                    return doGNRS(NAIP, self, buf);
+                case "PubSub Node":
+                    return doPubSubNode(NAIP, self, buf);
+                default:
+                    throw new IOException("don't  understand which element you want to create");
+            }
         }
+        return null;
     }
 
-    private PacketProcessorRouter doRouter(NA self, BufferedReader buf) throws IOException {
+    private PacketProcessor doRouter(HashMap<String, String> NAIP, BufferedReader buf) throws IOException {
         HashMap<NA, NetworkInterface> neighbor = new HashMap<>();
         HashMap<NA, NA> routing = new HashMap<>();
         HashMap<GUID, NA> localGUIDTable = new HashMap<>();
-        String line;
-        String[] address = buf.readLine().split(" ");
-        GUID GNRSguid = createGUID(address[0].getBytes());
-        NA GNRSna = new NA(Integer.getInteger(address[1]));
-        address = buf.readLine().split(" ");
-        GUID PubSubguid = createGUID(address[0].getBytes());
-        NA PubSubna = new NA(Integer.getInteger(address[1]));
-        while ((line = buf.readLine()) != null) {
-            address = line.split(" ");
-            if (self.getVal() == Integer.getInteger(address[0])) {
-                //update neighbor/routing/localGUIDTable
+        String[] address;
+        while ((address = buf.readLine().split(" ")) != null) {
+            switch (address.length) {
+                case 2:
+                    NAIP.put(address[0], address[1]);
+                    break;
+                case 4:
+                    //build localGUID table here
+                    if (self.getVal() == Integer.getInteger(address[2])) {
+                        localGUIDTable.put(createGUID(address[0].getBytes()), new NA(Integer.getInteger(address[1])));
+                    }
+                    break;
+                case 5:
+                    //build neighbor table here
+                    AddNeighbors(neighbors, new NA(Integer.getInteger(address[2])), new NA(Integer.getInteger(address[0])));
+                    if (self.getVal() == Integer.getInteger(address[0])) {
+                        //update neighbor/routing/localGUIDTable
+                        NA nbr = new NA(Integer.getInteger(address[2]));
+                        neighbor.put(nbr,
+                                new NetworkInterfaceUDP(
+                                        new InetSocketAddress(NAIP.get(address[0]), Integer.getInteger(address[1])),
+                                        new InetSocketAddress(NAIP.get(address[2]), Integer.getInteger(address[3]))));
+                        routing.put(nbr, nbr);
+                    } else if (self.getVal() == Integer.getInteger(address[2])) {
+                        NA nbr = new NA(Integer.getInteger(address[0]));
+                        neighbor.put(nbr,
+                                new NetworkInterfaceUDP(
+                                        new InetSocketAddress(NAIP.get(address[2]), Integer.getInteger(address[3])),
+                                        new InetSocketAddress(NAIP.get(address[0]), Integer.getInteger(address[1]))));
+                        routing.put(nbr, nbr);
+                    }
+                    break;
+                default:
+                    throw new IOException("topology file format is incorrect!");
             }
+        }
+        //build routing table here
+        for (NA nbr : routing.keySet()) {
+            LookForR(routing, nbr);
         }
         return new PacketProcessorRouter(GNRSna, localGUIDTable, routing, self, neighbor);
     }
 
-    private PacketProcessor doEndHost(NA self, BufferedReader buf) {
+    private PacketProcessor doEndHost(HashMap<String, String> NAIP, NA self, BufferedReader buf) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private PacketProcessor doGNRS(NA self, BufferedReader buf) {
+    private PacketProcessor doGNRS(HashMap<String, String> NAIP, NA self, BufferedReader buf) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private PacketProcessor doPubSubNode(NA self, BufferedReader buf) {
+    private PacketProcessor doPubSubNode(HashMap<String, String> NAIP, NA self, BufferedReader buf) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void initial(String[] address, BufferedReader buf) throws IOException {
+        GNRSguid = createGUID(address[0].getBytes());
+        GNRSna = new NA(Integer.getInteger(address[1]));
+        address = buf.readLine().split(" ");
+        //PubSub node
+        PubSubguid = createGUID(address[0].getBytes());
+        PubSubna = new NA(Integer.getInteger(address[1]));
+    }
+
+    private void AddNeighbors(HashMap<NA, ArrayList<NA>> neighbors, NA na1, NA na2) {
+        ArrayList<NA> tmp1 = neighbors.get(na1);
+        ArrayList<NA> tmp2 = neighbors.get(na2);
+        if (tmp1 == null) {
+            neighbors.put(na1, tmp1 = new ArrayList<>());
+        }
+        if (tmp2 == null) {
+            neighbors.put(na2, tmp2 = new ArrayList<>());
+        }
+        tmp1.add(na2);
+        tmp2.add(na1);
+    }
+
+    private void LookForR(HashMap<NA, NA> routing, NA nbr) {
+        for (NA i : neighbors.get(nbr)) {
+            if (!routing.containsKey(i) && i.equals(self)) {
+
+            }
+        }
     }
 
     private class PacketProcessorTopologyManager extends PacketProcessor {
 
         private final PacketProcessor processor;
 
-        public PacketProcessorTopologyManager(PacketProcessor processor, String tracefile) throws FileNotFoundException {
-            super(processor.getNa(), processor.getNeighbors());//processor neighbor and topomanager neighbor is different
+        public PacketProcessorTopologyManager(HashMap<NA, NetworkInterface> neighbor, PacketProcessor processor, String tracefile) throws FileNotFoundException {
+            super(processor.getNa(), neighbor);//one neighbor with ctrl center
             extractSelfTrace(new BufferedReader(new FileReader(tracefile)));
             this.processor = processor;
         }
